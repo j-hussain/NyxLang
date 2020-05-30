4e018546cdda32200997f0b3b0c6448778bbbbe2
pr0tege <jabirhussain@protonmail.ch>
2020-05-30 16:15:29 +0100

    added a bit to the scanner class, hoping to complete a basic program for it tonight

--- src/com/nyx/Scanner.java
+++ src/com/nyx/Scanner.java
@@ -9,13 +9,45 @@ import static com.nyx.nyx.TokenIdentifier.*;
 
 class Scanner {
 	/*
-	docstring here
+	Scanner class for lexical analysis
+
 	 */
 	private final List<Token> tokens = new ArrayList<>();
-	private final String source;
+	private final String instruction;
 
 	// Variables used for the offsets of the string format of the lexeme being analysed
 	private int line_number = 1;
 	private int initial = 0;
 	private int current = 0;
+
+	private void evaluateTokens() {
+		char character = increment();
+		switch (character) {
+			case ';':
+				createToken(SEMICOLON);
+				break;
+
+		}
+	}
+
+	Scanner(String instruction) {
+		this.instruction = instruction;
+	}
+
+	List<Token> evaluateTokens() {
+		while (!endOfLine) {
+			initial = current;
+			evaluateTokens();
+		}
+		// Token(line no, lexeme, literal, type
+		tokens.add(new Token(line_number, "", null, EOF));
+		return tokens;
+	}
+
+	private boolean endOfLine() {
+		return current >= instruction.length();
+	}
+
+
+
 }
